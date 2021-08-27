6285c2f7883ea9c6675b59f78ece99f939e06f78
Dario D'Amico <dd'amico@esri.com>
2021-08-27 09:47:52 -0700

    sr

--- src/apps/currents.ts
+++ src/apps/currents.ts
@@ -26,26 +26,35 @@
 //  import FeatureLayer from "esri/layers/FeatureLayer";
 import TileLayer from "esri/layers/TileLayer";
 import Color from "esri/Color";
-import { VectorFieldFlowSource } from "../flow/sources";
-import { MapUnits } from "../core/types";
-import { Field, PixelsPerSecond } from "../flow/types";
+import ImageryTileLayer from "esri/layers/ImageryTileLayer";
+// import { VectorFieldFlowSource } from "../flow/sources";
+// import { MapUnits } from "../core/types";
+// import { Field, PixelsPerSecond } from "../flow/types";
  
- // Tell the worker frameworks the location of the modules.
- esriConfig.workers.loaderConfig = {
-   packages: [
-     {
-       name: "animated-flow-ts",
-       location: location.origin + "/demos/js"
-     }
-   ]
- };
- 
- // A tile layer is used as basemap.
- const tileLayer = new TileLayer({
-   url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/Spilhaus_Vibrant_Basemap/MapServer",
-   effect: "saturate(10%) brightness(0.3)"
- });
- 
+// Tell the worker frameworks the location of the modules.
+esriConfig.workers.loaderConfig = {
+  packages: [
+    {
+      name: "animated-flow-ts",
+      location: location.origin + "/demos/js"
+    }
+  ]
+};
+
+// A tile layer is used as basemap.
+const tileLayer = new TileLayer({
+  url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/Spilhaus_Vibrant_Basemap/MapServer",
+  effect: "saturate(10%) brightness(0.3)"
+});
+
+const currentsLayer = new ImageryTileLayer({
+  url: "https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/Spilhaus_UV_ocean_currents/ImageServer"
+});
+
+const temperatureLayer = new ImageryTileLayer({
+  url: "https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/HyCOM_Surface_Temperature___Spilhaus/ImageServer"
+});
+
 //  // TODO: Change this.
 //  const temperatureLayer = new FeatureLayer({
 //    url: "https://services.arcgis.com/DO4gTjwJVIJ7O9Ca/arcgis/rest/services/Unacast_Latest_Available__Visitation_and_Distance_/FeatureServer",
@@ -74,42 +83,46 @@ import { Field, PixelsPerSecond } from "../flow/types";
 
 
 
+// https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/Spilhaus_UV_ocean_currents/ImageServer
 
-// A vortex vector field.
-function createVortex(vortexCenter: [MapUnits, MapUnits]): Field {
-  return (x, y) => {
-    x -= vortexCenter[0];
-    y -= vortexCenter[1];
-    const d2 = x * x + y * y;
-    return [-1000000.0 * y / d2, -1000000.0 * x / d2];
-  };
-}
 
-// We create 3 vortices over the US.
-const vortex1 = createVortex([-10000000, 4000000]);
-const vortex2 = createVortex([-20000000, -6000000]);
-const vortex3 = createVortex([1000000, 0]);
+// // A vortex vector field.
+// function createVortex(vortexCenter: [MapUnits, MapUnits]): Field {
+//   return (x, y) => {
+//     x -= vortexCenter[0];
+//     y -= vortexCenter[1];
+//     const d2 = x * x + y * y;
+//     return [-1000000.0 * y / d2, -1000000.0 * x / d2];
+//   };
+// }
 
-const windVectorField = (x: MapUnits, y: MapUnits): [PixelsPerSecond, PixelsPerSecond] => {
-  const v1 = vortex1(x, y);
-  const v2 = vortex2(x, y);
-  const v3 = vortex3(x, y);
+// // We create 3 vortices over the US.
+// const vortex1 = createVortex([-10000000, 4000000]);
+// const vortex2 = createVortex([-20000000, -6000000]);
+// const vortex3 = createVortex([1000000, 0]);
 
-  // The three vortex velocity fields can be simply added together.
-  return [v1[0] + v2[0] + v3[0], v1[1] + v2[1] + v3[1]];
-};
+// const windVectorField = (x: MapUnits, y: MapUnits): [PixelsPerSecond, PixelsPerSecond] => {
+//   const v1 = vortex1(x, y);
+//   const v2 = vortex2(x, y);
+//   const v3 = vortex3(x, y);
+
+//   // The three vortex velocity fields can be simply added together.
+//   return [v1[0] + v2[0] + v3[0], v1[1] + v2[1] + v3[1]];
+// };
 
 // The `FlowLayer` uses the analytic velocity field as data source.
-const windLayer = new FlowLayer({
-  source: new VectorFieldFlowSource(windVectorField),
-  effect: "bloom(1.5, 0.5px, 0.2)",
-  opacity: 0.7,
-  useWebWorkers: true,
+// console.log(FlowLayer, Color);
+const flowLayer = new FlowLayer({
+  // source: new VectorFieldFlowSource(windVectorField),
+  url: "https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/Spilhaus_UV_ocean_currents/ImageServer",
+  // effect: "bloom(1.5, 0.5px, 0.2)",
+  // opacity: 0.7,
+  // useWebWorkers: true,
   color: new Color([255, 100, 80, 1])
 } as any);
 
 const map = new EsriMap({
-  layers: [tileLayer, windLayer]
+  layers: [tileLayer, temperatureLayer, currentsLayer, flowLayer]//, windLayer]
 });
 
 
