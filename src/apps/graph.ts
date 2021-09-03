594af56485db2f60e931deed8f22eb61d23247ba
Dario D'Amico <dd'amico@esri.com>
2021-09-03 16:20:36 -0700

    wip

---
+++ src/apps/graph.ts
@@ -0,0 +1,200 @@
+/*
+  Copyright 2021 Esri
+  Licensed under the Apache License, Version 2.0 (the "License");
+  you may not use this file except in compliance with the License.
+  You may obtain a copy of the License at
+    http://www.apache.org/licenses/LICENSE-2.0
+  Unless required by applicable law or agreed to in writing, software
+  distributed under the License is distributed on an "AS IS" BASIS,
+  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+  See the License for the specific language governing permissions and
+  limitations under the License.
+*/
+
+/**
+ * @module animated-flow-ts/apps/winds
+ *
+ * An app that uses real UV wind data from an imagery tile layer and combines
+ * using blend modes with another layer.
+ */
+
+ import EsriMap from "esri/Map";
+ import MapView from "esri/views/MapView";
+ import { FlowLayer } from "../flow/layer";
+ import esriConfig from "esri/config";
+import TileLayer from "esri/layers/TileLayer";
+import ImageryTileLayer from "esri/layers/ImageryTileLayer";
+import GroupLayer from "esri/layers/GroupLayer";
+import GraphicsLayer from "esri/layers/GraphicsLayer";
+import Graphic from "esri/Graphic";
+import SimpleFillSymbol from "esri/symbols/SimpleFillSymbol";
+import Polygon from "esri/geometry/Polygon";
+import { FlowSettings } from "../flow/settings";
+import MultipartColorRamp from "esri/tasks/support/MultipartColorRamp";
+import AlgorithmicColorRamp from "esri/tasks/support/AlgorithmicColorRamp";
+import Color from "esri/Color";
+import { VectorFieldRenderer } from "esri/rasterRenderers";
+ 
+// Tell the worker frameworks the location of the modules.
+esriConfig.workers.loaderConfig = {
+  packages: [
+    {
+      name: "animated-flow-ts",
+      location: location.origin + "/demos/js"
+    }
+  ]
+};
+
+// A tile layer is used as basemap.
+const tileLayer = new TileLayer({
+  url: "https://tiles.arcgis.com/tiles/nGt4QxSblgDfeJn9/arcgis/rest/services/Spilhaus_Vibrant_Basemap/MapServer",
+  // effect: "saturate(10%) brightness(0.3)"
+  effect: "saturate(10%) brightness(0.3)"
+});
+
+const clipLayer = new GraphicsLayer({
+  blendMode: "destination-in"
+});
+
+const ex = new Polygon({
+  spatialReference: {
+    wkid: 54099
+  },
+  rings: [
+    [
+      [
+          -16781936.102334447,
+          16603476.57387844
+      ],
+      [
+          16687922.504049376,
+          16603476.57387844
+      ],
+      [
+          16687922.504049376,
+          -16734090.101254879
+      ],
+      [
+          -16781936.102334447,
+          -16734090.101254879
+      ],
+      [
+          -16781936.102334447,
+          16603476.57387844
+      ]
+    ]
+  ]
+});
+
+clipLayer.add(new Graphic({
+  geometry: ex,
+  symbol: new SimpleFillSymbol({
+    color: "white"
+  })
+}));
+
+const basemapLayer = new GroupLayer();
+basemapLayer.add(tileLayer);
+basemapLayer.add(clipLayer);
+
+const colorRamp = new MultipartColorRamp({
+  colorRamps: [
+    new AlgorithmicColorRamp({
+      fromColor: new Color([20, 100, 150, 255]),
+      toColor: new Color([70, 0, 150, 255])
+    }),
+    new AlgorithmicColorRamp({
+      fromColor: new Color([70, 0, 150, 255]),
+      toColor: new Color([170, 0, 120, 255])
+    }),
+    new AlgorithmicColorRamp({
+      fromColor: new Color([170, 0, 120, 255]),
+      toColor: new Color([230, 100, 60, 255])
+    }),
+    new AlgorithmicColorRamp({
+      fromColor: new Color([230, 100, 60, 255]),
+      toColor: new Color([255, 170, 0, 255])
+    }),
+    new AlgorithmicColorRamp({
+      fromColor: new Color([255, 170, 0, 255]),
+      toColor: new Color([255, 255, 0, 255])
+    }),
+  ]
+});
+
+const temperatureLayer = new ImageryTileLayer({
+  url: "https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/HyCOM_Surface_Temperature___Spilhaus/ImageServer",
+  renderer: {
+    colorRamp,
+    "computeGamma": false,
+    "gamma": [1],
+    "useGamma": false,
+    "stretchType": "min-max",
+    "type": "raster-stretch"
+  }
+});
+
+const settings = new FlowSettings();
+settings.smoothing = 5;
+settings.fixedCellSize = 5;
+// settings.speedScale = 1;
+// settings.smoothing = 1;
+// settings.linesPerVisualization = 10000;
+// settings.fixedCellSize = 1;
+// settings.lineWidth = 2;
+// settings.verticesPerLine = 100;
+// settings.segmentLength = 1;
+
+const flowLayer = new FlowLayer({
+  url: "https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/Spilhaus_UV_ocean_currents/ImageServer",
+  useWebWorkers: true,
+  blendMode: "destination-in",
+  settings
+} as any);
+
+console.log(temperatureLayer);
+
+// We create a group layer to combine temperature and wind in a single visualization
+// where the temperature drives the color of the streamlines.
+const groupLayer = new GroupLayer({
+  effect: "bloom(1.5, 0.5px, 0.2)"
+});
+groupLayer.add(temperatureLayer);
+groupLayer.add(flowLayer);
+
+console.log(basemapLayer, groupLayer, flowLayer);
+
+const vectors = new ImageryTileLayer({
+  url: "https://tiledimageservices.arcgis.com/jIL9msH9OI208GCb/arcgis/rest/services/Spilhaus_UV_ocean_currents/ImageServer"
+});
+
+vectors.renderer = VectorFieldRenderer.fromJSON({"visualVariables":[{"type":"sizeInfo","field":"Magnitude","maxDataValue":7.4870004653930655,"maxSize":70,"minDataValue":3.6020002365112305,"minSize":30},{"type":"rotationInfo","field":"Direction","rotationType":"arithmetic"}],"attributeField":"Magnitude","flowRepresentation":"flow_from","rotationType":"arithmetic","style":"single_arrow","symbolTileSize":50,"type":"vectorField"}) as any;
+
+const map = new EsriMap({
+  // layers: [basemapLayer, groupLayer]
+  // layers: [groupLayer]
+  layers: [vectors]
+  // layers: [groupLayer]
+  // layers: [basemapLayer]
+  // layers: [groupLayer]
+});
+
+setTimeout(() => {
+  console.log("vectors.renderer", JSON.stringify(vectors.renderer.toJSON()));
+}, 10000);
+
+// Create the map view.
+const view = new MapView({
+  container: "viewDiv",
+  map,
+  scale: 80000000,
+  center: [-98, 39],
+  // constraints: {
+  //   minScale: 80000000
+  // }
+});
+
+view.on("pointer-down", (evt) => {
+  const mapPoint = view.toMap({ x: evt.x, y: evt.y });
+  console.log(mapPoint);
+});
